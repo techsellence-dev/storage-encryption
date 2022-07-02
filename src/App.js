aad9b501194281ad899f154d70294d24d311b24b
Aparup Ganguly <aparupganguly86@gmail.com>
2022-07-02 15:45:37 +0530

    day2 react convert

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import sha256 from 'crypto-js/sha256';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+import Base64 from 'crypto-js/enc-base64';
+import {useEffect} from "react"
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256")
+var CryptoJS = require("crypto-js");
+
+function Myhash(event) {
+  event.preventDefault();
+  const user = {
+    name :{name}.toString(),
+    psd : {psd}.toString(),
+  };
+
+
+  var name = document.getElementById("name").value;
+  var psd = document.getElementById("psd").value;
+  let string = JSON.stringify(user);
+
+  const secret = "Hello123";
+  let encrypted = CryptoJS.AES.encrypt(string, secret).toString();
+  var bytes = CryptoJS.AES.decrypt(encrypted, secret);
+
+  console.log(encrypted);
+
+  localStorage.setItem("user", encrypted);
+  var originalText = bytes.toString(CryptoJS.enc.Utf8).toString();;
+ console.log(originalText);
+}
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form text-align="center" onSubmit={Myhash}>
+          <div>
+            <label> Name :</label>
+            <input type="text" id="name" required />
+            <div>
+              <label> Password : </label>
+              <input type="text" id="psd" required />
+            </div>
+
+            <div>
+              <input type="submit" />
+            </div>
+          </div>
+        </form>
       </header>
     </div>
   );
