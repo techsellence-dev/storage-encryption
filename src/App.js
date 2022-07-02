a3b3be734a40b50dc0a3efd239f20d3906d85848
gazalmighty <98213852+techsellence-dev@users.noreply.github.com>
2022-07-02 16:23:37 +0530

    encryption-decryption

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
-import sha256 from 'crypto-js/sha256';
-import hmacSHA512 from 'crypto-js/hmac-sha512';
-import Base64 from 'crypto-js/enc-base64';
-import {useEffect} from "react"
+import sha256 from "crypto-js/sha256";
+import hmacSHA512 from "crypto-js/hmac-sha512";
+import Base64 from "crypto-js/enc-base64";
+import { useEffect } from "react";
 var AES = require("crypto-js/aes");
-var SHA256 = require("crypto-js/sha256")
+var SHA256 = require("crypto-js/sha256");
 var CryptoJS = require("crypto-js");
 
 function Myhash(event) {
   event.preventDefault();
-  var namelabel = document.getElementById("name").value;
-  var psdlabel = document.getElementById("psd").value;
-  const user = {
-    name :namelabel,
-    psd : psdlabel,
-  };
-console.log(user)
-
-  
-  let string = JSON.stringify(user);
-  console.log(string);
+  let namelabel = document.getElementById("name").value;
+  let psdlabel = document.getElementById("psd").value;
   const secret = "Hello123";
-  let encrypted = CryptoJS.AES.encrypt(string, secret).toString();
-  var bytes = CryptoJS.AES.decrypt(encrypted, secret);
+  let jsonObjectToString;
+  let retrieveLocal;
+  const user = {
+    name: namelabel,
+    psd: psdlabel,
+  };
+  console.log(` object is`);
+  console.log(user);
 
+  jsonObjectToString = JSON.stringify(user);
+  console.log(`Json object to String is ${jsonObjectToString}`);
+
+  let encrypted = CryptoJS.AES.encrypt(jsonObjectToString, secret).toString();
   console.log(encrypted);
-
   localStorage.setItem("user", encrypted);
-  var originalText = bytes.toString(CryptoJS.enc.Utf8).toString();;
- console.log(originalText);
+  retrieveLocal=localStorage.getItem("user")
+  var retrieveLocalDecrypt = CryptoJS.AES.decrypt(retrieveLocal, secret);
+  var originalText = retrieveLocalDecrypt.toString(CryptoJS.enc.Utf8).toString();
+  console.log(originalText);
 }
 
 function App() {
-
-
   return (
     <div className="App">
       <header className="App-header">
